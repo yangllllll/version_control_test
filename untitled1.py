1b56b78e53050a72246b2d3be49964a5b2298229
Liulin Yang <yllygq@gmail.com>
2018-01-27 10:12:16 +0800

    cleaning

--- untitled1.py
+++ untitled1.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 27 00:55:05 2018
-
-@author: yll
-"""
-
-for j in range(1,5):
-    print(j)
+from nltk.stem import WordNetLemmatizer as wnl
+wnl=wnl()
+a=wnl.lemmatize("dogs")
